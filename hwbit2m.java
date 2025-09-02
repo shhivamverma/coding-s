87cdde34a473f972e4e53c35f2fb99b9d7ffcf5b
SHIVAM VERMA <shivamvermaknp9922@gmail.com>
2025-09-02 23:31:59 +0530

    Add files via upload

---
+++ hwbit2m.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class hwbit2m
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n=sc.nextInt();
+        if((n&(n-1)) == 0)
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+    }
+}
